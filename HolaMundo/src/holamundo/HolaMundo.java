5516020704cfd592b155422aa394886c50aac766
alopezcobo <alopezcobo@atanasoff04.danielcastelao.org>
2016-10-03 09:55:31 +0200

    Pues la verdad es que no he cambiado nada :/

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -16,7 +16,7 @@ package holamundo;
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hola mundos");
+        System.out.print("Hola mundo");
     }
     
 }
